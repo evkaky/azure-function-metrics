dc7c15ff01f128e3a2311d032162554add29cab3
Mykola Ilminsky <Mykola.Ilminsky-ext@ab-Inbev.com>
2023-05-29 13:15:13 +0300

    init sample app

--- src/main/java/org/example/functions/HttpTriggerJava.java
+++ src/main/java/org/example/functions/HttpTriggerJava.java
@@ -1,40 +1,25 @@
 package org.example.functions;
 
-import com.microsoft.azure.functions.*;
-import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
-import org.slf4j.LoggerFactory;
+import io.micrometer.core.instrument.Metrics;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-/**
- * Azure Functions with HTTP Trigger.
- */
+@Component
 public class HttpTriggerJava {
-    /**
-     * This function listens at endpoint "/api/HttpTriggerJava". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpTriggerJava
-     * 2. curl {your host}/api/HttpTriggerJava?name=HTTP%20Query
-     */
+
     @FunctionName("HttpTriggerJava")
     public HttpResponseMessage run(
-        @HttpTrigger(
-            name = "req",
-            methods = {HttpMethod.GET, HttpMethod.POST},
-            authLevel = AuthorizationLevel.FUNCTION
-        ) HttpRequestMessage<Optional<String>> request, ExecutionContext context) {
-
-        LoggerFactory.getLogger(HttpTriggerJava.class).info("hello1");
-
-        // Parse query parameter
-        String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
-
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
-        }
+        @HttpTrigger(name = "req", methods = HttpMethod.GET)
+        HttpRequestMessage<Optional<String>> request
+    ) {
+        Metrics.counter("my_counter").increment();
+        return request.createResponseBuilder(HttpStatus.OK).body("Hello").build();
     }
 }
